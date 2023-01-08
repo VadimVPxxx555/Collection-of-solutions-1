3064dda726fafd9ec3b927d0f27d85c6bbc8045e
Vadim V P <Everestxxx555@yandex.ru>
2023-01-08 23:03:26 +0300

    коммит Zadacha_44

---
+++ Zadacha_44/Program.cs
@@ -0,0 +1,28 @@
+﻿// Не используя рекурсию выведите число фибоначи до N
+
+Console.Clear();
+
+int DataEnter(string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+void Fibonachi(int n)
+{
+    int a = 0;
+    int b = 1;
+    int f = 0;
+    Console.Write("0 1 ");
+    for (int i = 2; i < n; i++)
+    {
+        f = a+b;  // 2 + 3
+        Console.Write(f + " ");
+        a = b;
+        b = f;
+    }
+}
+
+int n = DataEnter("Введите число ");
+Fibonachi(n);
