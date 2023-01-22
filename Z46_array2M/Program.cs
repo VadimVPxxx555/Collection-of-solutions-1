c2760a11425e93041a3c6637d7bad1df64f3c82b
Vadim V P <Everestxxx555@yandex.ru>
2023-01-22 22:20:05 +0300

    коммит2 Z46_array2M

--- Z46_array2M/Program.cs
+++ Z46_array2M/Program.cs
@@ -5,12 +5,46 @@
 // 5 -2 33 -2
 // 77 3 8 1
 
+Console.Clear();
+
+void ShowArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] CreateRandomArray(int rows, int columns, int leftRange, int rightRange)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i,j] = new Random().Next(leftRange, rightRange + 1);
+        }
+    }
+    return array;
+}
+
+int EnterNumber(string message)  //message - сообщение
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
 // получить от пользователя M и N 
 int m = EnterNumber("введите значение m ");
 int n = EnterNumber("введите значение n ");
 
 // создать двумерный массив и заполнить 
-int[,] matrix = CreateRandomArray(m,n); // Создать случайный массив
+int[,] matrix = CreateRandomArray(m, n, 1, 9); // Создать случайный массив
 
 // вывести массив в консоль
 ShowArray(matrix); // Показать массив
